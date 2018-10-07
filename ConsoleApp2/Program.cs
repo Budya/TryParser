c12bccb010c8ebab8b3ce1f521ea9b05850e2811
Дима <Budya2012@gmail.com>
2018-10-07 18:33:07 +0300

    Before WEB part

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.Extensions;
@@ -23,23 +25,119 @@ namespace ConsoleApp2
             //service.HideCommandPromptWindow = true;
             //service.LoadImages = false;
             var service = ChromeDriverService.CreateDefaultService(Environment.CurrentDirectory);
-
             var options = new ChromeOptions();
             //options.AddArguments("--headless");
 
+            List<string> sourceUrls = new List<string>();
+            List<Shoes> resultListShoes = new List<Shoes>();
+
             IWebDriver driver = new ChromeDriver(service, options);
-            driver.Url = @"https://www.walmart.com/browse/clothing/mens-shoes/5438_1045804_1045807";
+            driver.Url = @"https://www.walmart.com/browse/clothing/mens-shoes/5438_1045804_1045807?page=1&ps=48#searchProductResult";
 
             WebDriverWait waitForElement = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            waitForElement.Until(ExpectedConditions.ElementIsVisible(By.ClassName("GlobalFooterCopyright-text")));
+            waitForElement.Until(ExpectedConditions.ElementIsVisible(By.ClassName("GlobalFooterCopyright")));
 
-            IWebElement elem = driver.FindElement(By.ClassName("GlobalFooterCopyright-text"));
+            IWebElement elem = driver.FindElement(By.ClassName("GlobalFooterCopyright"));
 
             driver.ExecuteJavaScript("arguments[0].scrollIntoView(true);", elem);
 
-            IWebElement element = driver.FindElement(By.ClassName("search-result-gridview-items"));
-            string pageHtml = driver.PageSource.;
-            //var text = element.ToString();
+            // get count of pages for parsing
+            var pagesWebElements = driver.FindElement(By.ClassName("paginator-list")).FindElements(By.TagName("li"));
+            var lastPage = pagesWebElements[pagesWebElements.Count-1].FindElement(By.TagName("a")).Text;
+            
+
+            // !!!!!!! ONLY FOR TEST
+            //int lastPageNum = Int32.Parse(lastPage);
+            int lastPageNum = 5;
+
+
+            // creating a list of urls
+            string urlFirstPart = @"https://www.walmart.com/browse/clothing/mens-shoes/5438_1045804_1045807?page=";
+            string urlEndPart = @"&ps=48#searchProductResult";
+            for (int pageIterator = 1; pageIterator <= lastPageNum; pageIterator++)
+            {
+                string walmartUrl = urlFirstPart + pageIterator + urlEndPart;
+                sourceUrls.Add(walmartUrl);
+            }
+
+            foreach (var sourceUrl in sourceUrls)
+            {
+                driver.Url = sourceUrl;
+
+                WebDriverWait waitForElement1 = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                waitForElement1.Until(ExpectedConditions.ElementIsVisible(By.ClassName("GlobalFooterCopyright")));
+                IWebElement elem1 = driver.FindElement(By.ClassName("GlobalFooterCopyright"));
+
+                driver.ExecuteJavaScript("arguments[0].scrollIntoView(true);", elem1);
+
+                var elements = driver.FindElement(By.ClassName("search-result-gridview-items"))
+                    .FindElements(By.ClassName("Grid-col"));
+
+
+
+                foreach (var webElement in elements)
+                {
+                    Shoes shoes =  new Shoes();
+
+                    Console.WriteLine(webElement.FindElement(By.TagName("img"))
+                        .GetAttribute("data-image-src")); //ссылка на изображение
+                    shoes.ShoesImageUrl = webElement.FindElement(By.TagName("img"))
+                        .GetAttribute("data-image-src");
+                    try
+                    {
+                        Console.WriteLine(webElement.FindElement(By.ClassName("product-brand"))
+                            .FindElement(By.TagName("strong")).Text);
+
+                        shoes.ShoesBrand = webElement.FindElement(By.ClassName("product-brand"))
+                            .FindElement(By.TagName("strong")).Text;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("No Brand Name");
+                        shoes.ShoesBrand = "No Brand Name";
+                        //throw;
+                    }
+                    
+                    Console.WriteLine(webElement.FindElement(By.TagName("img"))
+                        .GetAttribute("alt")); // название ботинок
+
+                    shoes.ShoesTitle = webElement.FindElement(By.TagName("img"))
+                        .GetAttribute("alt");
+
+                    try
+                    {
+                        var selectors = webElement.FindElement(By.ClassName("swatch-selector")).FindElements(By.TagName("button"));
+                        foreach (var selector in selectors)
+                        {
+                            Console.WriteLine(selector.FindElement(By.TagName("img")).GetAttribute("alt"));
+                            string str = selector.FindElement(By.TagName("img")).GetAttribute("alt");
+                            shoes.ShoesVariants.Add(str);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        //Console.WriteLine(e.Message);
+                        
+                        shoes.ShoesVariants.Add("No Variants");
+                    }
+
+                    try
+                    {
+                        Console.WriteLine(webElement.FindElement(By.ClassName("price-group")).GetAttribute("aria-label"));
+                        shoes.ShoesPrice = webElement.FindElement(By.ClassName("price-group"))
+                            .GetAttribute("aria-label");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Out of Stock");
+                        shoes.ShoesPrice = "Out of Stock";
+                        //throw;
+                    }
+                    
+                    resultListShoes.Add(shoes);
+                }
+
+            }
 
             Console.ReadKey();
         }
---
+++ ConsoleApp2/Shoes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Shoes
+    {
+        public string ShoesImageUrl { get; set; }
+        public List<string> ShoesVariants { get; set; }
+        public string ShoesBrand { get; set; }
+        public string ShoesTitle { get; set; }
+        public string ShoesPrice { get; set; }
+
+        public Shoes()
+        {
+            ShoesVariants = new List<string>();
+        }
+    }
+}
